c9333fe08838529e131419589a681471192158dc
YutaekKim <86652701+YutaekKim@users.noreply.github.com>
2022-08-09 13:59:48 +0900

    Add files via upload

---
+++ src/main/java/com/example/swaggertest/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.swaggertest.controller;
+
+@RestController
+public class HelloController {
+
+    @Operation(summary = "test hello", description = "hello api example")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK !!"),
+            @ApiResponse(responseCode = "400", description = "BAD REQUEST !!"),
+            @ApiResponse(responseCode = "404", description = "NOT FOUND !!"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR !!")
+    })
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello(@Parameter(description = "이름", required = true, example = "Park") @RequestParam String name) {
+        return ResponseEntity.ok("hello " + name);
+    }
+}
