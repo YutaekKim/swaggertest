28bff0f5c35ae6f74b1e63b6dd16193088af69f4
dbxor <rladbxor@gmail.com>
2022-08-10 10:43:41 +0900

    Swagger 3.0.0 Spring Security

--- src/main/java/com/example/swaggertest/config/SwaggerConfig.java
+++ src/main/java/com/example/swaggertest/config/SwaggerConfig.java
@@ -4,14 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-
-@OpenAPIDefinition(
-        info = @Info(title = "API 명세서",
-                description = "API 명세서 테스트 입니다.",
-                version = "v0.1"))
 @Configuration
 public class SwaggerConfig {
-
+/*
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
@@ -30,4 +25,5 @@ public class SwaggerConfig {
                 .version("1.0")
                 .build();
     }
+ */
 }
